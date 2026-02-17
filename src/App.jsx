93482a98ab6814f1a245fee2b2f6c65931ef4d55
naveen kumar <nk143barodiya@gmail.com>
2026-02-17 13:59:08 +0530

    add the last commit...

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 
+
 export default function App() {
+  useEffect(() => {
+    fetch("https://travel-backend-plan.onrender.com/");
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
